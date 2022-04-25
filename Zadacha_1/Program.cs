128d6f67df0c2062790a55193866113f6c9b68db
Nikita <parnikaao@mail.ru>
2022-04-25 22:08:12 +0700

    add tasks

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу вычисления модуля числа;
+
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+if (a>0)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    a=-1*a;
+    Console.WriteLine(a);
+}
---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+string s_b = Console.ReadLine();
+int b = int.Parse(s_b);
+
+Console.WriteLine($"{a}>{b}?");
+Console.WriteLine($"{a > b}");
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
