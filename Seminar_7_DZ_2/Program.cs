496c694f1e04f54af303b9faad956438bf7d9278
Nikita <parnikaao@mail.ru>
2022-06-01 20:48:15 +0700

    update readme and add homework seminar 7

---
+++ Seminar_7_DZ_2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++) Console.Write($"\t{arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void ReturnNum(int[,] arr, int arg1, int arg2)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    string answer = "element doesn't exist";
+
+    if (arg1 < 0 | arg1 > row | arg2 < 0 | arg2 > column)
+        Console.WriteLine(answer);
+
+    else
+        Console.WriteLine($"Value in position {(arg1, arg2)} ---> = {arr[arg1, arg2]}");
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+Console.Write("Enter an argument1: ");
+int argument1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter an argument2: ");
+int argument2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] arr_1 = MassNums(row, column, 1, 5);
+Print(arr_1);
+ReturnNum(arr_1, argument1, argument2);
+
+
---
+++ Seminar_7_DZ_3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++) 
+        Console.Write($"\t{arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void Average(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    double sum=0;
+
+    for (int j = 0; j < column_size; j++)
+    {
+        for (int i = 0; i < row_size; i++) 
+        sum+= arr[i,j];
+        Console.WriteLine($"Average = {sum/row_size}");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+double[,] arr_1 = MassNums(row, column, 0, 10);
+Print(arr_1);
+Average(arr_1);
+
