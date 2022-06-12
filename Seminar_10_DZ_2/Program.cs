2c36e837235257f487e09658f9133ad6a9412a04
Nikita <parnikaao@mail.ru>
2022-06-12 11:35:55 +0700

    update readme and add homeworks seminar 10

---
+++ Seminar_10_DZ_2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Повторение матриц. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+
+
+void FillArray3D (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.Write($"x; y; z ---> ");
+      for (int k = 0; k < array3D.GetLength(2); k++)
+        Console.Write( $"({i}); ({j}); ({k}) ---> {array3D[i,j,k]}; ");
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void PrintArray3D(int[,,] array3D)
+{
+  int[] arr = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    arr[i] = new Random().Next(10, 100);
+    number = arr[i];
+    if (i >= 1)
+      for (int j = 0; j < i; j++)
+        while (arr[i] == arr[j])
+        {
+          arr[i] = new Random().Next(10, 100);
+          j = 0;
+          number = arr[i];
+        }
+          number = arr[i];
+  }
+  
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = arr[count];
+        count++;
+      }
+    }
+}
+
+int[,,] array3D = new int[3, 3, 3];
+PrintArray3D(array3D);
+FillArray3D(array3D);
---
+++ Seminar_10_DZ_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n. 
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else if (m > 0 && n > 0)  return Akkerman(m - 1, Akkerman(m, n - 1));
+    return 0;
+}
+Console.WriteLine(Akkerman(3, 2));
